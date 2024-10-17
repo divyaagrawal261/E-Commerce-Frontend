5455f8c5001f2e21cc71056bc60982cf35a0699d
siri <chandhana535@gmail.com>
2024-10-17 14:45:29 +0530

    responsive homepage

--- src/components/ShopTheLookCard.jsx
+++ src/components/ShopTheLookCard.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ShopTheLookCard({src, redirect}) {
+function ShopTheLookCard({ src, redirect }) {
   return (
-    <div className="w-[300px] aspect-square bg-cover shopTheLook flex justify-center items-center overflow-hidden" style={{ backgroundImage: `url(${src})` }} >
-      <div className="w-[300px] aspect-square  hidden justify-center items-center innerShopCard bg-[#0000007c]">
-        <Link to="/products" className='text-white p-2 border-2 border-white  hover:bg-[#0000004b]'>SHOP THE LOOK</Link>
+    <div
+      className="w-[150px] sm:w-[200px] md:w-[250px] aspect-square bg-cover shopTheLook flex justify-center items-center overflow-hidden relative"
+      style={{ backgroundImage: `url(${src})` }}
+    >
+      <div className="w-full h-full flex justify-center items-center absolute inset-0 innerShopCard bg-[#0000007c] opacity-0 hover:opacity-100 transition-opacity duration-300">
+        <Link to={redirect || '/products'} className='text-white bg-violet-500 p-2 rounded-lg font-medium'>
+          SHOP THE LOOK
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default ShopTheLookCard
+export default ShopTheLookCard;
